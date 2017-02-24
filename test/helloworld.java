8cc02adbbf55fb240eb29bd9ff92c7f1957ecd9b
levelicarus <levelicarus@gmail.com>
2017-02-23 19:27:36 -0500

    Adding a test repo

---
+++ test/helloworld.java
@@ -0,0 +1,26 @@
+public class HelloWorld {
+	public static void main(Strings args{}) {
+
+	}
+	public static boolean int test1() {
+
+	}
+
+	public static void test2(boolean b, float a) {
+	
+	}
+
+	public static float test3(int i, double d, float f) {
+	
+	}
+
+
+	public static ADT int test4(int i, float f, double d) {
+	
+	}
+
+
+	public static ADT test5(ADT a, ADT b, ADT c) {
+	
+	}
+}
